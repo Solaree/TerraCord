a884d2e83716dce68bb1381f5c128fae9c3ced93
Solar <115794865+Solaree@users.noreply.github.com>
2023-03-05 17:02:52 +0200

    Delete GUI directory

--- GUI/window.py
+++
@@ -1,9 +0,0 @@
-#from Packets.Client.sraki import *; from Packets.Client.change_status import *; from Packets.Client.send_message import *; from Packets.Client.dm_message import *; from Packets.Client.file_upload import *; from Packets.Client.online import *; from Packets.Client.reactions import *;  
-#from Packets.Server.get_dm import *; from Packets.Server.send_message import *; from Packets.Server.get_channels import *; from Packets.Server.get_message import *; from Packets.Server.get_servers import *; from Packets.Server.join_server import *; from Packets.Server.leave_server import *;
-
-from design import *
-
-app = QApplication(sys.argv)
-window = Window()
-window.show()
-sys.exit(app.exec())
